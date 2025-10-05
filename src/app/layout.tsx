e59a6e13a1e8c6a8d060a9ea0b6106832bd9d8d3
JoxAQC35 <jose.quispe35@unmsm.edu.pe>
2025-10-05 18:34:15 -0500

    Agregando icono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/neo.png" type="image/png" />
+        <link rel="icon" href="/icon.png" type="image/png" />
       </head>
       <body className="font-body antialiased">
         {children}
